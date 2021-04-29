1565329ffefeab79650b810e7dfacf61b885fc2f
zjw981119 <962610351@qq.com>
2021-04-29 23:46:55 +0800

    实现后端接收add请求后同时对统计表车辆运输次数进行插入或修改。

--- src/main/java/com/gradproject/server/controller/SelfCounterController.java
+++ src/main/java/com/gradproject/server/controller/SelfCounterController.java
@@ -1,6 +1,7 @@
 package com.gradproject.server.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.gradproject.server.entity.CumulationCounter;
 import com.gradproject.server.entity.SelfCounter;
 import com.gradproject.server.entity.model.SelfResponse;
 import com.gradproject.server.service.RfidService;
@@ -26,16 +27,31 @@ public class SelfCounterController {
     private RfidService rfidService;
 
     //上传车辆运输记录及图片
+    @GetMapping("/getData")
+    public SelfResponse getCountData(@RequestParam("beginTime") String beginTime,
+                                     @RequestParam("endTime") String endTime){
+        SelfResponse response = new SelfResponse();
+        try {
+            logger.info("开始时间：【{}】。", beginTime);
+            logger.info("结束时间：【{}】。", endTime);
+
+        } catch (Exception e){
+            logger.error("数据插入异常，异常信息为：【{}】", e.getMessage(), e);
+        }
+        return response.failure("数据插入异常");
+    }
+    //上传车辆运输记录及图片
     @PostMapping("/add")
     public SelfResponse receiveThirdPartyData(@RequestBody String counterString){
         SelfResponse response = new SelfResponse();
         try {
             SelfCounter counter = JSONObject.parseObject(counterString, SelfCounter.class);
+            CumulationCounter Cucounter=new CumulationCounter();
             logger.info("自研计数模块收到的数据为：【{}】", counter.toString());
 
             //todo 根据查询车辆相关信息
             //插入计数数据
-            response = counterService.insetCounterData(counter);
+            response = counterService.insetCounterData(counter,Cucounter);
             return response;
         } catch (Exception e){
             logger.error("数据插入异常，异常信息为：【{}】", e.getMessage(), e);
--- src/main/java/com/gradproject/server/service/SelfCounterService.java
+++ src/main/java/com/gradproject/server/service/SelfCounterService.java
@@ -1,7 +1,9 @@
 package com.gradproject.server.service;
 
 import com.alibaba.fastjson.JSON;
+import com.gradproject.server.dao.CumulationCounterMapper;
 import com.gradproject.server.dao.SelfCounterMapper;
+import com.gradproject.server.entity.CumulationCounter;
 import com.gradproject.server.entity.SelfCounter;
 import com.gradproject.server.entity.model.ReturnCode;
 import com.gradproject.server.entity.model.SelfResponse;
@@ -15,6 +17,7 @@ import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class SelfCounterService {
@@ -24,7 +27,10 @@ public class SelfCounterService {
     private FileService fileService;
 
     @Resource
-    private SelfCounterMapper mapper;
+    private SelfCounterMapper Smapper;
+
+    @Resource
+    private CumulationCounterMapper Cmapper;
 
     @Autowired
     private RfidService rfidService;
@@ -35,7 +41,7 @@ public class SelfCounterService {
      * @param counter
      * @return
      */
-    public SelfResponse insetCounterData(SelfCounter counter) {
+    public SelfResponse insetCounterData(SelfCounter counter, CumulationCounter Cucounter) {
         logger.info("插入一条计数数据，数据为：【{}】", JSON.toJSONString(counter));
         SelfResponse response = new SelfResponse();
         //必传参数不可为空
@@ -63,29 +69,33 @@ public class SelfCounterService {
             if (!StringUtils.isEmpty(address)) {
                 counter.setMiningArea(address.substring(0, address.contains("@") ? address.indexOf("@") : address.length()));
             }
-            //根据当前时间判定是白班还是夜班
-            //白班时间早八点-晚六点
+            //每一班时间为当天早上06:00:00-第二天早上05:59:59
             Date recordTime = DateUtils.getDate(counter.getTime(), DateUtils.FORMAT_LONG);
             //获取当前天 形如：yyyy-MM-dd
             String currentDay = DateUtils.getFormatDate(recordTime, DateUtils.FORMAT_SHORT);
             Date beginTime = DateUtils.getDate(currentDay + " 06:00:00", DateUtils.FORMAT_LONG);
             Date endTime = DateUtils.getDate(currentDay + " 05:59:59", DateUtils.FORMAT_LONG);
-            String queryBeginTime, queryEndTime;
+            String queryBeginTime, queryEndTime, workDay;
             //获得计数时间段
             if (recordTime.getTime() < beginTime.getTime()) {
                 //前一天06:00:00~今天05:59:59
                 Date preDay = DateUtils.addDays(recordTime, -1);
                 String preDayStr = DateUtils.getFormatDate(preDay, DateUtils.FORMAT_SHORT);
+                workDay=preDayStr;
                 queryBeginTime = preDayStr + " 06:00:00";
                 queryEndTime = DateUtils.getFormatDate(endTime, DateUtils.FORMAT_LONG);
             } else {
                 //今天06:00:00~第二天05:59:59
                 Date nextDay = DateUtils.addDays(recordTime, 1);
                 String nextDayStr = DateUtils.getFormatDate(nextDay, DateUtils.FORMAT_SHORT);
+                workDay=DateUtils.getFormatDate(beginTime,DateUtils.FORMAT_SHORT);
                 queryBeginTime = DateUtils.getFormatDate(beginTime, DateUtils.FORMAT_LONG);;
                 queryEndTime = nextDayStr + " 05:59:59";
             }
-            logger.info("当前车号：【{}】, 计数时间段为：【{}-{}】", carNum, queryBeginTime, queryEndTime);
+            //设置统计表的车号、运输日
+            Cucounter.setCarNum(carNum);
+            Cucounter.setWorkDay(workDay);
+            logger.info("当前车号：【{}】, 计数时间段为：【{}-{}】, 车辆工作日期为：【{}】", carNum, queryBeginTime, queryEndTime,workDay);
             //构造计数的查询条件
 //            SelfCounterExample example = new SelfCounterExample();
 //            SelfCounterExample.Criteria criteria = example.createCriteria();
@@ -93,9 +103,12 @@ public class SelfCounterService {
 //            criteria.andTimeBetween(DateUtils.getFormatDate(queryBeginTime, null),
 //                    DateUtils.getFormatDate(queryEndTime, null));
 //            criteria.andCardNoEqualTo(counter.getCardNo());
-            int counterDegree = mapper.selectCounts(counter.getRfid(), queryBeginTime, queryEndTime);
+            int counterDegree = Smapper.selectCounts(counter.getRfid(), queryBeginTime, queryEndTime);
             //设置次数
             counter.setSubCounts("" + (counterDegree + 1));
+            Cucounter.setCounts("" + (counterDegree + 1));
+
+
             /*
             空满载默认值 -1
             if (counter.getIsFull() == 0) {
@@ -103,13 +116,30 @@ public class SelfCounterService {
             }
              */
 
-            logger.info("插入数据库中的数据为：【{}】", JSON.toJSONString(counter));
-            int result = mapper.insertCounterData(counter);
-            if (result > 0) {
-                logger.info("自研计数统计数据插入成功！插入的id为：【{}】", counter.getId());
+            //向self_counter数据表中插入数据
+            logger.info("插入self_counter数据库中的数据为：【{}】", JSON.toJSONString(counter));
+            int Sresult = Smapper.insertCounterData(counter);
+            if (Sresult > 0) {
+                logger.info("数据插入self_counter表成功！插入的id为：【{}】", counter.getId());
             }
 
-            return response.success(counter.getId());
+            //向cumulation_counter数据表中插入数据
+            logger.info("插入cumulation_counter数据库中的数据为：【{}】", JSON.toJSONString(Cucounter));
+            List<CumulationCounter> CuCounterList=Cmapper.selectCumulateDataByNumAndDay(carNum,workDay);
+            //若为空，则插入数据，否则更新数据
+            int Cresult;
+            if(CuCounterList.isEmpty())
+            {
+                Cresult=Cmapper.insertCumulateData(Cucounter);
+            }
+            else{
+                Cresult=Cmapper.updateCumulateData(Cucounter);
+            }
+            if(Cresult>0){
+                logger.info("数据插入cumulation_counter表成功！");
+            }
+
+            return response.success();
 //            return response.success("OK");
         } catch (Exception e) {
             logger.error("数据库插入申请失败，异常原因为：【{}】", e.getMessage(), e);
