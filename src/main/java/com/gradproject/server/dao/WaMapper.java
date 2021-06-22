9f1fd6e429cd875d456927564c1ffe738754acd7
huangdan <3504439376@qq.com>
2021-06-22 16:51:06 +0800

    机械统计表内外部

--- src/main/java/com/gradproject/server/dao/WaMapper.java
+++ src/main/java/com/gradproject/server/dao/WaMapper.java
@@ -32,18 +32,22 @@ public interface WaMapper {
     })
     @Select({"<script>"
             +"SELECT "
-            +"car_type,car_id,sum(num ) as tripNum, oil_price,   "
-            +"beilv,((sum(num )) * oil_price * beilv) as zhuang, ((sum(num )) * beilv) as biao, "
-            +" (((sum(num )) * beilv) * ((sum(num )) * oil_price * beilv)) as fang, sum(jishishijian*jishidanjia) as jishi,((sum(num )) * oil_price) as mei,(sum(jishishijian*jishidanjia) + ((sum(num )) * oil_price) + ((sum(num )) * oil_price * beilv)) as mao, "
-            +" sum(oil_L) as oil_L,(oil_price * sum(oil_L)) as ran "
-            +"FROM re_waji "
+            +"C.car_type,C.car_id,(sum(num )) as tripNum, C.oil_price, "
+            +" yun,C.beilv,((sum(num )) * C.oil_price * C.beilv) as zhuang, ((sum(num )) * C.beilv) as biao, "
+            +" (((sum(num )) * C.beilv) * ((sum(num )) * C.oil_price * C.beilv)) as fang, sum(re_waji.jishishijian*re_waji.jishidanjia) as jishi,((sum(num )) * C.oil_price) as mei,(sum(re_waji.jishishijian*re_waji.jishidanjia) + ((sum(num )) * C.oil_price) + ((sum(num )) * C.oil_price * C.beilv)) as mao, "
+            +" sum(re_waji.oil_L) as oil_L,(C.oil_price * sum(re_waji.oil_L)) as ran "
+            +"FROM re_waji , "
+            +"(SELECT A.* FROM re_waji_config as A,(SELECT car_id,MAX(date) as maxdate FROM re_waji_config WHERE date <![CDATA[ >= ]]> DATE_FORMAT(#{value1},'%Y-%m-%d') AND date <![CDATA[ <= ]]> DATE_FORMAT(#{value2},'%Y-%m-%d') GROUP BY car_id) as B "
+            +"WHERE A.car_id = B.car_id AND A.date = B.maxdate and A.type = '外部' ) AS C "
             +"<where>"
-            +"<if test=\"value1 !=null and value1 !=''\"> and date <![CDATA[ >= ]]> DATE_FORMAT(#{value1},'%Y-%m-%d')</if>"
-            +"<if test=\"value2 !=null and value2 !=''\"> and date <![CDATA[ <= ]]> DATE_FORMAT(#{value2},'%Y-%m-%d')</if> "
+            +"<if test=\"value1 !=null and value1 !=''\"> and re_waji.date <![CDATA[ >= ]]> DATE_FORMAT(#{value1},'%Y-%m-%d')</if>"
+            +"<if test=\"value2 !=null and value2 !=''\"> and re_waji.date <![CDATA[ <= ]]> DATE_FORMAT(#{value2},'%Y-%m-%d')</if> "
+            +"and C.car_id = re_waji.car_id "
 
-            +"and type = '外部' "
+
+
             +"</where>"
-            +"GROUP BY car_id  "
+            +"GROUP BY C.car_id "
 
             +"</script>"})
 
@@ -72,23 +76,23 @@ public interface WaMapper {
     })
     @Select({"<script>"
             +"SELECT "
-            +"re_car_config.car_type, re_car_config.car_id,(count(gn_self_counter.transport_distance)+SUM(gn_self_counter.addcar_particular))  as tripNum,re_car_config.oil_price, "
-            +"gn_self_counter.transport_distance,re_car_config.multiple,((count(gn_self_counter.transport_distance)+SUM(gn_self_counter.addcar_particular)) *  re_car_config.oil_price  * re_car_config.multiple) as zhuang,((count(gn_self_counter.transport_distance)+SUM(gn_self_counter.addcar_particular)) *  re_car_config.multiple) as biao, "
-            +"FORMAT((((count(gn_self_counter.transport_distance)+SUM(gn_self_counter.addcar_particular)) *  re_car_config.multiple) * ((count(gn_self_counter.transport_distance)+SUM(gn_self_counter.addcar_particular)) *  re_car_config.multiple)),2) as fang ,re_car_config.jishi,((count(gn_self_counter.transport_distance)+SUM(gn_self_counter.addcar_particular)) * re_car_config.oil_price) as mei,(((count(gn_self_counter.transport_distance)+SUM(gn_self_counter.addcar_particular)) * re_car_config.oil_price) + jishi + ((count(gn_self_counter.transport_distance)+SUM(gn_self_counter.addcar_particular)) *  re_car_config.oil_price  * re_car_config.multiple)) as mao, "
-            +"sum(re_car_config.oil_L) as oil_L,(re_car_config.oil_price * sum(re_car_config.oil_L)) as ran,re_car_config.bind_excavator  "
-
-            +"FROM gn_self_counter,re_car_config "
+            +"C.car_type, gn_self_counter.car_no,(count(gn_self_counter.transport_distance)+SUM(gn_self_counter.addcar_particular))  as tripNum,C.oil_price, "
+            +"gn_self_counter.transport_distance,C.multiple,((count(gn_self_counter.transport_distance)+SUM(gn_self_counter.addcar_particular)) *  C.oil_price  * C.multiple) as zhuang,((count(gn_self_counter.transport_distance)+SUM(gn_self_counter.addcar_particular)) *  C.multiple) as biao, "
+            +"FORMAT((((count(gn_self_counter.transport_distance)+SUM(gn_self_counter.addcar_particular)) * C.multiple) * ((count(gn_self_counter.transport_distance)+SUM(gn_self_counter.addcar_particular)) *  C.multiple)),2) as fang ,C.jishi,((count(gn_self_counter.transport_distance)+SUM(gn_self_counter.addcar_particular)) * C.oil_price) as mei,(((count(gn_self_counter.transport_distance)+SUM(gn_self_counter.addcar_particular)) * C.oil_price) + jishi + ((count(gn_self_counter.transport_distance)+SUM(gn_self_counter.addcar_particular)) *  C.oil_price  * C.multiple)) as mao, "
+            +"sum(C.oil_L) as oil_L,(C.oil_price * sum(C.oil_L)) as ran,C.car_id ,C.bind_excavator  "
+            +"FROM gn_self_counter, "
+            +"(SELECT A.* FROM re_car_config as A,(SELECT car_id,MAX(date) as maxdate FROM re_car_config WHERE date <![CDATA[ >= ]]> DATE_FORMAT(#{value1},'%Y-%m-%d') AND date <![CDATA[ <= ]]> DATE_FORMAT(#{value2},'%Y-%m-%d') GROUP BY car_id) as B "
+            +"WHERE A.car_id = B.car_id AND A.date = B.maxdate  and A.type = '外部') AS C "
             +"<where>"
             +"<if test=\"value1 !=null and value1 !=''\"> and time <![CDATA[ >= ]]> #{value1}</if>"
             +"<if test=\"value2 !=null and value2 !=''\"> and time <![CDATA[ <= ]]> #{value2}</if> "
-            +"and gn_self_counter.car_no = re_car_config.car_id "
+            +"and gn_self_counter.car_no = C.car_id "
             +"and gn_self_counter.address = 'PANH@001' "
-            +"and date_format(time,'%Y-%m-%d') = date  "
 
-            +"and re_car_config.type = '外部' "
+
             +"and gn_self_counter.transport_distance is not null "
             +"</where>"
-            +"GROUP BY gn_self_counter.transport_distance,re_car_config.car_id ,re_car_config.bind_excavator  "
+            +"GROUP BY gn_self_counter.transport_distance,C.car_id ,C.bind_excavator "
 
             +"</script>"})
     List<Waing> selectW(@Param("value1") String value1, @Param("value2") String value2);
